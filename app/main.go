4afb7a2e771e34736f2e0042bc92b7676041cc5a
Jason Nicholls <jasonnicholls1981@gmail.com>
2026-05-08 12:45:32 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,9 +7,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
 	fmt.Print("$ ")
 	reader := bufio.NewReader(os.Stdin)
